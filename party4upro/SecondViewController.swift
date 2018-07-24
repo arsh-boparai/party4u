780d6559eb62438ef8968886ced6c16a6042faa3
Arsh <arsh@arshdeeps-macbook-pro.local>
2018-07-24 03:08:12 -0400

    added map

--- party4upro/SecondViewController.swift
+++ party4upro/SecondViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
-
-class SecondViewController: UIViewController {
-
+import MapKit
+import CoreLocation
+class SecondViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager:CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        determineMyCurrentLocation()
+    }
+    
+    
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        var poiCoodinates = CLLocationCoordinate2D()
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        poiCoodinates.latitude = userLocation.coordinate.latitude
+        poiCoodinates.longitude = userLocation.coordinate.longitude
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        let viewRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(poiCoodinates, 750, 750)
+        self.mapView.setRegion(viewRegion, animated: true)
+        // Plot pin
+        let pin: MKPointAnnotation = MKPointAnnotation()
+        pin.coordinate = poiCoodinates
+        self.mapView.addAnnotation(pin)
+        
+        //add title to the pin
+//        pin.title = selectedLocation!.name
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
+    }
 }
-
